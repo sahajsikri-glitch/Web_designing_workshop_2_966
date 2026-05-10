798ce061607275791205cf61d347cac0b3bc2886
sahajsikri-glitch <sahajsikri@gmail.com>
2026-05-10 20:00:45 +0530

    values and variable

--- jonas-udemy/script2.js
+++ jonas-udemy/script2.js
@@ -1,7 +1,34 @@
 ////////////////////////////////////
 // Linking a JavaScript File
 let js = "amazing";
-if (js === "amazing") alert("javascript is fun!");
+// if (js === "amazing") alert("javascript is fun!");
 console.log(40 + 8 + 23 - 10);
 
-//////////////////////////////////////
+////////////////////////////////////
+
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+// way -1 (this one's better)
+let myFirstJob = "Coder"; //camel case notation
+let myCurrentJob = "Teacher";
+
+// way -2
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
