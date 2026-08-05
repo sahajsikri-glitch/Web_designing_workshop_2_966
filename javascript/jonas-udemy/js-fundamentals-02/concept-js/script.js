4f4fe016b437906e315ee87a14c5662ea65ed568
sahajsikri-glitch <sahajsikri@gmail.com>
2026-08-05 17:22:06 +0530

    coding-challenge-03

--- javascript/jonas-udemy/js-fundamentals-02/concept-js/script.js
+++ javascript/jonas-udemy/js-fundamentals-02/concept-js/script.js
@@ -93,21 +93,21 @@ const calcAge = function (birthYeah) {
   return 2037 - birthYeah;
 };
 
-const yearsUntilRetirement = function (birthYeah, firstName) {
-  const age = calcAge(birthYeah);
-  const retirement = 65 - age;
+// const yearsUntilRetirement = function (birthYeah, firstName) {
+//   const age = calcAge(birthYeah);
+//   const retirement = 65 - age;
 
-  if (retirement > 0) {
-    console.log(`${firstName} retires in ${retirement} years`);
-    return retirement;
-  } else {
-    console.log(`${firstName} has already retired 🎉`);
-    return -1;
-  }
-};
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired 🎉`);
+//     return -1;
+//   }
+// };
 
-console.log(yearsUntilRetirement(1991, "Jonas"));
-console.log(yearsUntilRetirement(1950, "Mike"));
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
 
 ///////////////////////////////////////
 // Introduction to Arrays
@@ -139,159 +139,132 @@ console.log(jonas);
 console.log(jonas.length);
 
 // Exercise
-const calcAge = function (birthYear) {
-  return 2037 - birthYeah;
-};
-const years = [1990, 1967, 2002, 2010, 2018];
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYeah;
+// };
+// const years = [1990, 1967, 2002, 2010, 2018];
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
 
-const ages = [
-  calcAge(years[0]),
-  calcAge(years[1]),
-  calcAge(years[years.length - 1]),
-];
-console.log(ages);
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[1]),
+//   calcAge(years[years.length - 1]),
+// ];
+// console.log(ages);
 
 /////////////////////////////////////
 // Basic Array Operations (Methods)
-const friends = ["Michael", "Steven", "Peter"];
+const friendsJi = ["Michael", "Steven", "Peter"];
 
 // Add elements
-const newLength = friends.push("Jay");
-console.log(friends);
+const newLength = friendsJi.push("Jay");
+console.log(friendsJi);
 console.log(newLength);
 
-friends.unshift("John");
-console.log(friends);
+friendsJi.unshift("John");
+console.log(friendsJi);
 
 // Remove elements
-friends.pop(); // Last
-const popped = friends.pop();
+friendsJi.pop(); // Last
+const popped = friendsJi.pop();
 console.log(popped);
-console.log(friends);
+console.log(friendsJi);
 
-friends.shift(); // First
-console.log(friends);
+friendsJi.shift(); // First
+console.log(friendsJi);
 
-console.log(friends.indexOf("Steven"));
-console.log(friends.indexOf("Bob"));
+console.log(friendsJi.indexOf("Steven"));
+console.log(friendsJi.indexOf("Bob"));
 
-friends.push(23);
-console.log(friends.includes("Steven"));
-console.log(friends.includes("Bob"));
-console.log(friends.includes(23));
+friendsJi.push(23);
+console.log(friendsJi.includes("Steven"));
+console.log(friendsJi.includes("Bob"));
+console.log(friendsJi.includes(23));
 
-if (friends.includes("Steven")) {
+if (friendsJi.includes("Steven")) {
   console.log("You have a friend called Steven");
 }
 
-/////////////////////////////////////
-// Coding Challenge #2
-
-/*
-Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
-
-1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
-2. And now let's use arrays! So create an array 'bills' containing the test data below.
-3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
-4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
-
-TEST DATA: 125, 555 and 44
-
-HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
-
-GOOD LUCK 😀
-*/
-
-/*
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-
-console.log(bills, tips, totals);
-
-
-///////////////////////////////////////
 // Introduction to Objects
 const jonasArray = [
-  'Jonas',
-  'Schmedtmann',
+  "Jonas",
+  "Schmedtmann",
   2037 - 1991,
-  'teacher',
-  ['Michael', 'Peter', 'Steven']
+  "teacher",
+  ["Michael", "Peter", "Steven"],
 ];
 
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  age: 2037 - 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven']
-};
+// const jonasy = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   age: 2037 - 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+//order doesnt matter in objects
 
-
-///////////////////////////////////////
 // Dot vs. Bracket Notation
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
+const jonasyla = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
   age: 2037 - 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven']
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
 };
-console.log(jonas);
+console.log(jonasyla);
 
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
+console.log(jonasyla.lastName);
+console.log(jonasyla["lastName"]);
 
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
+const nameKey = "Name";
+console.log(jonasyla["first" + nameKey]);
+console.log(jonasyla["last" + nameKey]);
 
 // console.log(jonas.'last' + nameKey)
 
-const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends",
+);
 
-if (jonas[interestedIn]) {
-  console.log(jonas[interestedIn]);
+if (jonasyla[interestedIn]) {
+  console.log(jonasyla[interestedIn]); //WHY NO " " IN INTERESTIN?
+  // because prompt take input in string form !
 } else {
-  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job, and friends",
+  );
 }
 
-jonas.location = 'Portugal';
-jonas['twitter'] = '@jonasschmedtman';
-console.log(jonas);
+jonasyla.location = "Portugal";
+jonasyla["twitter"] = "@jonasylaschmedtman";
+console.log(jonasyla);
 
 // Challenge
-// "Jonas has 3 friends, and his best friend is called Michael"
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
-
+// "jonasyla has 3 friends, and his best friend is called Michael"
+console.log(
+  `${jonasyla.firstName} has ${jonasyla.friends.length} friends, and his best friend is called ${jonasyla.friends[0]}`,
+);
 
 ///////////////////////////////////////
 // Object Methods
 
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
+const sahaju = {
+  firstName: "sahaju",
+  lastName: "Schmedtmann",
   birthYeah: 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
   hasDriversLicense: true,
 
   // calcAge: function (birthYeah) {
   //   return 2037 - birthYeah;
   // }
 
-  // calcAge: function () {
+  // calcAge2: function () {
   //   // console.log(this);
   //   return 2037 - this.birthYeah;
   // }
@@ -302,20 +275,19 @@ const jonas = {
   },
 
   getSummary: function () {
-    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
-  }
+    return `${this.firstName} is a ${this.calcAge()}-year old ${sahaju.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
 };
 
-console.log(jonas.calcAge());
+console.log(sahaju.calcAge());
 
-console.log(jonas.age);
-console.log(jonas.age);
-console.log(jonas.age);
+console.log(sahaju.age);
+console.log(sahaju.age);
+console.log(sahaju.age);
 
 // Challenge
-// "Jonas is a 46-year old teacher, and he has a driver's license"
-console.log(jonas.getSummary());
-*/
+// "sahaju is a 46-year old teacher, and he has a driver's license"
+console.log(sahaju.getSummary());
 
 ///////////////////////////////////////
 // Coding Challenge #3
